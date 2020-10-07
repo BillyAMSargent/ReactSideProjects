b0dd8550dcf70109468ee856a857248d23a3fc4e
BillyAMSargent <billy@schoolcomms.co.uk>
2020-10-07 16:02:18 +0100

    response time take 2

--- React/group-survey/client/src/App.tsx
+++ React/group-survey/client/src/App.tsx
@@ -9,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload test BIG OOF test response time.
+          Edit <code>src/App.tsx</code> and save to reload test BIG OOF test response time but this time im paying attention.
         </p>
         <a
           className="App-link"
